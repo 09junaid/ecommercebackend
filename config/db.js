6e42f3146fb619e76eb369f4e4e89df5f62d3921
Junaid Arshad <junaidarshad647@gmail.com>
2025-05-16 16:01:14 +0500

    backend updateing env and db

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB=async()=>{
   try {
-   const conn= await mongoose.connect(process.env.DB_URL,{
-    dbName:"ecommerceapp",
-   })
+   const conn= await mongoose.connect(process.env.DB_URL);
     console.log(`DB Connection Success ${conn.connection.host}`.bgGreen.white)
   } catch (error) {
     console.log(`DB Connection Failed ${error.message}`.bgRed.white)
